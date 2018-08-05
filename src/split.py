74b5bf78764c330f3a9fff1a78150eeeade44bbc
snailccc <765743536@qq.com>
2018-08-05 16:26:54 +0800

    正在解决编码过多，撑爆了内存的问题

--- src/split.py
+++ src/split.py
@@ -3,16 +3,21 @@ import pandas as pd
 def split_data(name):
     data=pd.read_csv('../data/{0}_set.csv'.format(name))
     label=data.pop('class')
-    for i in range(1,19):
+    print(label.shape)
+    for i in range(1,20):
         label_temp=[]
         for j in range(label.shape[0]):
-            if label.iloc[j,0] == j:
+            if label.iloc[j] == i:
                 label_temp.append(1)
             else:
                 label_temp.append(0)
+        label_temp=pd.DataFrame(label_temp,columns=['label'])
         data_temp=data.join(label_temp)
-        data_temp.to_csv('../data/splited_data/name_{0}_splited.csv'.format(i),index=False)
-        print('name_{0} is finished'.format(i))
+        print(data_temp.info())
+        data_temp.to_csv('../data/splited_data/name_{0}_splited.csv'.format(str(i)),index=False)
+        data_temp=None
+        label_temp=None
+        print('name_{0} is finished'.format(str(i)))
     print('splited is ok')
 
 
